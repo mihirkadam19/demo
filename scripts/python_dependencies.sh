3c38bd123f3291f5b4fdd4d5eb20fe31c7f3c881
mihirkadam19 <mihirkadam85@yahoo.in>
2023-05-25 10:15:38 +0530

    scripts to deploy

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,1 +1,17 @@
 pwd > /home/ubuntu/python_dep_o.txt
+
+cd /home/ubuntu/ 
+
+sudo apt install python3-pip
+
+sudo apt install python3-virtualenv
+
+virtualenv venv
+
+source venv/bin/activate
+
+cd p2_newsproject
+
+pip install -r requirements.txt > /home/ubuntu/python_dep.sh
+
+deactivate
--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,2 +1,7 @@
 pwd > /home/ubuntu/start_app_o.txt
 
+cd /home/ubuntu
+
+source venv/bin/activate
+
+gunicorn p2_newsproject.wsgi:application -c gunicorn.conf.py > /home/ubuntu/start_app_o.txt
