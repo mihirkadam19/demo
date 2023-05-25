b605d7ec8f6c0910e453d7964021b719c48f3aaa
mihirkadam19 <mihirkadam85@yahoo.in>
2023-05-25 13:15:22 +0530

    scripts to deploy v9.0

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -2,4 +2,6 @@ pwd > /home/ubuntu/start_app_o.txt
 
 cd /home/ubuntu/p2_newsproject
 
-sudo nohup gunicorn p2_newsproject.wsgi:application -c gunicorn.conf.py 
+sudo gunicorn p2_newsproject.wsgi:application -c gunicorn.conf.py &
+
+echo "good"
